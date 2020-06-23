bde9b82e6b77972a1a17e82c4b2e799e4b4577a3
成浩 <957869975@qq.com>
2020-06-23 16:36:35 +0800

    feat: 增加防抖处理,wait=300

--- src/components/code-area/index.vue
+++ src/components/code-area/index.vue
@@ -8,6 +8,7 @@
   </div>
 </template>
 <script>
+import debounce from 'lodash/debounce'
 import Ace from 'ace-builds'
 import 'ace-builds/webpack-resolver' // 在 webpack 环境中使用必须要导入
 
@@ -40,9 +41,12 @@ export default {
       value: this.value ? this.value : ''
     })
     this.ace.setTheme('ace/theme/chrome')
+
     this.ace.getSession().setMode('ace/mode/markdown')
     this.ace.renderer.setShowPrintMargin(false)
-    this.ace.getSession().on('change', this.handleOnChange)
+
+    const handleChangeEvent = debounce(this.handleOnChange, 300)
+    this.ace.getSession().on('change', handleChangeEvent)
   },
   methods: {
     handleOnChange() {
