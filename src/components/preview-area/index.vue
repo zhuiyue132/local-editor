db23989e1825e92c4ea57d6d0e93ef466246cb3f
成浩 <957869975@qq.com>
2020-06-24 18:32:38 +0800

    feat: 增加github corner

--- src/components/preview-area/index.vue
+++ src/components/preview-area/index.vue
@@ -1,9 +1,17 @@
 <template>
-  <div v-html="value" class="md-preview-area"></div>
+  <div class="md-preview-area">
+    <github-corner style="position: absolute; top: 50px; border: 0; right: 0;"></github-corner>
+    <div v-html="value"></div>
+  </div>
 </template>
 <script>
+import githubCorner from '../github-corner'
+
 export default {
   name: 'PreviewArea',
+  components: {
+    githubCorner
+  },
   props: {
     value: {
       type: String,
