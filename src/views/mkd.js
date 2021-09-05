b39d2fb6ce18954bd6a1c77293bc2cff27bd3d8b
芬达Tz <fenda.job@gmail.com>
2021-09-05 21:15:34 +0800

    fix: 调整七牛云CNAME域名

--- src/views/mkd.js
+++ src/views/mkd.js
@@ -25,12 +25,6 @@ const mkd = new Markdown({
   .use(require('markdown-it-ins'))
   .use(require('markdown-it-mark'))
   .use(require('markdown-it-task-checkbox'))
-  .use(require('markdown-it-texmath'), {
-    // eslint-disable-next-line global-require
-    engine: require('katex'),
-    delimiters: 'dollars',
-    katexOptions: { macros: { '\\RR': '\\mathbb{R}' } }
-  })
   .use(require('markdown-it-container'))
   .use(require('markdown-it-plantuml'), {
     imageFormat: 'png',
