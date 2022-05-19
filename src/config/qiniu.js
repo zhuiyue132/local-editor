6608987eb84a0843979934193c693c3c5153a560
芬达Tz <fenda.job@gmail.com>
2022-05-19 23:10:57 +0800

    update

--- src/config/qiniu.js
+++ src/config/qiniu.js
@@ -2,13 +2,13 @@
  * @Author: chenghao
  * @Date: 2020-08-13 10:22:22
  * @Last Modified by: chenghao
- * @Last Modified time: 2020-12-28 16:58:04
+ * @Last Modified time: 2022-05-19 22:45:39
  * @Desc: 七牛云的一些参数配置
  */
 
 export default {
   AccessKey: 'WFV4UXP7y1xhb97VB0beS8fprQ13nv9grbHw_O1c',
   SecretKey: 'zktTQv54Ko1rfmaYJemqeACrkAN6GAXatjPObHHb',
-  bucket: 'zhuiyue',
-  cdnAddress: 'http://img.plutos.net.cn/'
+  bucket: 'zhuiyue-outwall',
+  cdnAddress: 'http://image.zhuiyue.work/'
 }
